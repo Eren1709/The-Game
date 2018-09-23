890a87acb9671b81cf8b62cc8c18ec2dc7b8b2b7
Eren1709 <42830927+Eren1709@users.noreply.github.com>
2018-09-23 21:54:47 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,139 @@
+		window.confirm("klik op ok om te beginnen met de hoofdsteden quiz")
+
+		{var punt = -1;
+		punt += 1;
+
+		
+			
+
+		var vraag1 = prompt ('De hoofdstad van Nederland is Amsterdam (waar of niet waar?)');
+		if (vraag1 == "waar") {
+		window.alert ("Correct!");
+		punt += 1;
+		}
+		else {
+			window.alert("Fout!");
+		}
+
+		var vraag2 = prompt ('De hoofdstad van Turkije is Istanbul (waar of niet waar?)');
+		if (vraag2 == "waar") {
+		window.alert ("fout!");
+		}
+		else {
+			window.alert("Correct!");
+			punt += 1;
+
+		}
+
+		var vraag3 = prompt ('De hoofdstad van Rusland is Sint-Petersburg (waar of niet waar?)');
+		if (vraag3 == "waar") {
+		window.alert ("fout!");
+		}
+		else {
+			window.alert("Correct!");
+			punt += 1;
+		}
+	
+		var vraag4 = prompt ('De hoofdstad van Oostenrijk is Wenen (waar of niet waar?)');
+		if (vraag4 == "waar") {
+		window.alert ("Correct!");
+		punt += 1;
+		}
+		else {
+			window.alert("Fout!");
+		}
+		
+		var vraag5 = prompt ('De hoofdstad van Portugal is Lissabon (waar of niet waar?)');
+		if (vraag5 == "waar") {
+		window.alert ("Correct!");
+		punt += 1;
+		}
+		else {
+			window.alert("Fout!");
+		}
+		
+		var vraag6 = prompt ('De hoofdstad van Suriname is Lima (waar of niet waar?)');
+		if (vraag6 == "waar") {
+		window.alert ("Fout!");
+		}
+		else {
+			window.alert("Correct!");
+			punt += 1;
+		}
+		
+		var vraag7 = prompt ('De hoofdstad van Duitsland is Brussel (waar of niet waar?)');
+		if (vraag7 == "waar") {
+		window.alert ("Fout!");
+		}
+		else {
+			window.alert("Correct!");
+			punt += 1;
+		}
+
+		var vraag8 = prompt ('De hoofdstad van Noorwegen is Helsinki (waar of niet waar?)');
+		if (vraag8 == "waar") {
+		window.alert ("Fout!");
+		}
+		else {
+			window.alert("Correct!");
+			punt += 1;
+		}
+
+		var vraag9 = prompt ('De hoofdstad van Zweden is Stockholm (waar of niet waar?)');
+		if (vraag9 == "waar") {
+		window.alert ("Correct!");
+		punt += 1;
+		}
+		else {
+			window.alert("Fout!");
+		}
+
+		var vraag10 = prompt ('De hoofdstad van Verenigd Koninkrijk is Dublin (waar of niet waar?)');
+		if (vraag10 == "waar") {
+		window.alert ("Fout!");
+		}
+		else {
+			window.alert("Correct!");
+			punt += 1;
+		}
+	}
+
+		document.write("<h1> Je antwoorden </h1> ")
+		document.write("<p1> vraag: De hoofdstad van Nederland is Amsterdam</p1> <br>")
+		document.write("je antwoord: "+vraag1);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Turkije is Istanbul</p1> <br>")
+		document.write("je antwoord: "+vraag2);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Rusland is Sint-Petersburg</p1> <br>")
+		document.write("je antwoord: "+vraag3);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Oostenrijk is Wenen</p1> <br>")
+		document.write("je antwoord: "+vraag4);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Portugal is Lissabon</p1> <br>")
+		document.write("je antwoord: "+vraag5);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Suriname is Lima</p1> <br>")
+		document.write("je antwoord: "+vraag6);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Duitsland is Brussel</p1> <br>")
+		document.write("je antwoord: "+vraag7);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Noorwegen is Helsinki</p1> <br>")
+		document.write("je antwoord: "+vraag8);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Zweden is Stockholm</p1> <br>")
+		document.write("je antwoord: "+vraag9);
+document.write("<br>----------------------------------------------")
+		document.write("<br><p1> vraag: De hoofdstad van Verenigd Koninkrijk is Dublin</p1> <br>")
+		document.write("je antwoord: "+vraag10);
+document.write("<br>----------------------------------------------")
+		
+document.write("<br> totaal goed: " + " " + punt)
+	
+
+		
+		
+	
+
